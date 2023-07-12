907e19fd44ad13b6e21fb540b9e6a9d7abaa221d
Psm0015 <pedro.mota@df.estudante.senai.br>
2023-07-12 11:17:13 -0300

    Aula09

---
+++ Aula09/Aula09.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Aula09
+{
+    static void Main()
+    {
+        int num=10;
+
+        Console.WriteLine(num<<1);//Dobra o Valor (20)
+        Console.WriteLine(num>>1);//Diminue pela metade (5)
+        Console.WriteLine(num<<2);//Dobra o Valor 2 vezes (40)
+        Console.WriteLine(num>>2);//Diminue pela metade 2 vezes(2.5 arrendado para (2))
+    }
+}
