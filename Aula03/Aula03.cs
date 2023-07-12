1fc1c3a5a85f3e8064b195999f1c6eb5c54f7441
Psm0015 <pedro.mota@df.estudante.senai.br>
2023-07-12 09:17:29 -0300

    aula 03

--- Aula03/Aula03.cs
+++ Aula03/Aula03.cs
@@ -3,7 +3,22 @@ using System;
 class Aula03{
 
     static void Main(){
-        
+        // Tipos Primitivos
+        // byte n1=10; // 0 e 255
+        // int num = 0; //Número Inteiro
+        // char letra = 'c'; //Caractere
+        // float valor= 5.3f; //Ponto Flutuante
+        // string nome="Pedro"; //Conjunto de Caracteres
+
+        int num1=0,num2=0,res=0;
+
+        num1=10;
+        num2=2;
+        res=num1 * num2;
+
+        // var aux=10; //Tipo definido na compilação
+
+        Console.WriteLine("A multiplicação de "+num1+" + "+num2+" é igual a:"+res);
     }
 
 }
