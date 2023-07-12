0b9b278fb4a82106664fae03293968bdc227cd0e
Psm0015 <pedro.mota@df.estudante.senai.br>
2023-07-12 10:51:02 -0300

    Aula07

---
+++ Aula07/Aula07.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Aula07{
+    static void Main(){
+        const string canal="Pedro Mota";//Nunca Poderá ter o valor mudado
+        const double pi=3.1415;//Nunca Poderá ter o valor mudado
+
+        Console.WriteLine("Canal {0}\nPi: {1}",canal,pi);
+    }
+}
