3565f2c620afc6fa703f248a1f02924098918aaf
pedro-mota <pedro.mota@df.estudante.senai.br>
2023-07-16 19:54:32 -0300

    Aula29

--- Aula29/Aula29.cs
+++ Aula29/Aula29.cs
@@ -1,83 +1,39 @@
+
 using System;
-using System.Collections.Generic;
+public class Jogador
+{
+    public int energia;
+    public bool vivo;
+
+    public string nome;
+
+    public Jogador(string n){
+        energia=100;
+        vivo=true;
+        nome=n;
+    }
+
+    ~Jogador(){
+        Console.WriteLine("Jogador {0} foi destruido",nome);
+    }
+}
 
 class Aula29
 {
     static void Main()
     {
-        int qtd = inicio();
-        int Boss = 1000;
-        List<Jogador> jogadores = new List<Jogador>();
-        jogadores = Jogadores(qtd);
-        Console.Write(imprime(jogadores));
 
-    }
+        string nome1;
+        Console.Write("Digite o Nome do Jogador: ");
+        nome1=Console.ReadLine();
+        Jogador j1=new Jogador(nome1);
+        Jogador j2=new Jogador("Théo");
 
-    static string imprime(List<Jogador> jogadores)
-    {
-        int x=1;
-        string st="";
-        foreach (Jogador j in jogadores)
-        {
-            st += "===========JOGADOR "+x+"===========";
-            st += "\nNome:" + j.nome;
-            st += "\nEnergia:" + j.energia;
-            st += "\nVida:"+j.vida;
-            x++;
-        }
-        return st;
-    }
+        // j1.energia = 50;
+        Console.WriteLine("Nome do Jogador 1: {0}",j1.nome);
+        Console.WriteLine("Nome do Jogador 2: {0}",j2.nome);
+        // Console.WriteLine("Energia do Jogador 1: {0}",j1.energia);
+        // Console.WriteLine("Energia do Jogador 2: {0}",j2.energia);
 
-    static int inicio()
-    {
-        int totJogadores = 0;
-        do
-        {
-            Console.WriteLine("Quantos jogadores vão jogar(MAX 4):");
-            totJogadores = int.Parse(Console.ReadLine());
-        } while (totJogadores < 1 | totJogadores > 4);
-
-        return totJogadores;
-    }
-
-    static List<Jogador> Jogadores(int n)
-    {
-        List<Jogador> jogadores = new List<Jogador>();
-
-        for (int i = 0; i < n; i++)
-        {
-            Console.WriteLine("\n============================");
-            Console.WriteLine("Nome do Jogador {0}:", i + 1);
-            Jogador j = new Jogador(Console.ReadLine());
-            jogadores.Add(j);
-
-        }
-        return jogadores;
-    }
-    static List<Jogador> atacar(List<Jogador> jogadores,ref int Boss){
-
-        Random random = new Random();
-        atackpower= random.Next(40,60);
-        atackpower *= jogadores.Length;
-        Boss -= atackpower;
-        foreach(Jogador j in jogadores){
-            j.energia -= atackpower*0.5;
-        }
-
-        return jogadores;
-    }
-}
-
-class Jogador
-{
-    public int energia;
-    public int vida;
-    public string nome;
-
-    public Jogador(String nm)
-    {
-        energia = 100;
-        vida = 100;
-        nome = nm;
     }
 }
---
+++ Aula30/Aula30.cs
@@ -0,0 +1,7 @@
+using System;
+
+class Aula30{
+     static void Main(){
+        Console.WriteLine("Hello World!");
+     }
+}
