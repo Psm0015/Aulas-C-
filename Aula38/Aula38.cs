c8c438021b4d5fcfffc408c51f3c5f849b21afa2
Psm0015 <pedro.mota@df.estudante.senai.br>
2023-07-17 09:04:52 -0300

    Aula38

---
+++ Aula38/Aula38.cs
@@ -0,0 +1,41 @@
+using System;
+
+class Base{
+    public Base(){
+        Console.WriteLine("Construtor da classe Base");
+    }
+
+    virtual public void info(){
+        Console.WriteLine("Base");
+    }
+
+}
+
+class Derivada1:Base{
+    public Derivada1(){
+        Console.WriteLine("Construtor da classe Derivada1");
+    }
+
+    override public void info(){
+        Console.WriteLine("Derivada1");
+    }
+}
+class Derivada2:Derivada1{
+    public Derivada2(){
+        Console.WriteLine("Construtor da classe Derivada2");
+    }
+    override public void info(){
+        Console.WriteLine("Derivada2");
+    }
+}
+
+class Aula38{
+    static void Main(){
+        Base Ref;
+        Derivada1 d1 = new Derivada1();
+        Derivada2 d2 = new Derivada2();
+
+        Ref=d1;
+        Ref.info();
+    }
+}
