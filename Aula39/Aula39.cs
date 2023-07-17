9989f624e3d082215f22f387b7c795ae6f88350d
Psm0015 <pedro.mota@df.estudante.senai.br>
2023-07-17 09:28:06 -0300

    Aula39

---
+++ Aula39/Aula39.cs
@@ -0,0 +1,39 @@
+using System;
+
+class Aula39{
+    static void Main(){
+        Carro carro1=new Carro();
+        carro1.aceleracao(1);
+        carro1.aceleracao(-1);
+        Console.WriteLine(carro1.getVelAtual());
+    }
+}
+
+abstract class Veiculo{//Classe base abstrata(pode somente ser herdada)
+    protected int velMaxima;
+    protected int velAtual;
+    protected bool ligado;
+
+    public Veiculo(){
+        ligado=false;
+        velAtual=0;
+    }
+    public void setLigado(bool ligado){
+        this.ligado=ligado;
+    }
+    public int getVelAtual(){
+        return velAtual;
+    }
+
+    abstract public void aceleracao(int mult);
+
+}
+
+class Carro:Veiculo{
+    public Carro(){
+        velMaxima = 120;
+    }
+    override public void aceleracao(int mult){
+        velAtual+=10*mult;
+    }
+}
