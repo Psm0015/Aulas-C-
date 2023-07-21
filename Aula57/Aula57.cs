a7c16f798b73465e7af61d52968580dfe5551c04
Psm0015 <pedro.mota@df.estudante.senai.br>
2023-07-21 15:15:23 -0300

    Aula57

--- Aula57/Aula57.cs
+++ Aula57/Aula57.cs
@@ -4,20 +4,32 @@ using System.Collections.Generic;
 class Aula57{
     static void Main(){
         List<string> carros= new List<string>();
-        List<string> carros2= new List<string>();
+        string[] carros2= new string[10];
 
         carros.Add("Golf");
         carros.Add("HRV");
         carros.Add("Focus");
         carros.Add("Argo");
 
-        carros2.AddRange(carros);
+        // carros2.AddRange(carros);
 
-        carros.Clear();
+        // carros.Clear();
+
+        carros.CopyTo(carros2,2);
+        if(carros.Contains("Golf")){
+            Console.WriteLine("Está na lista");
+        }else{
+            Console.WriteLine("Não encontrado!");
+        }
 
         foreach (string c in carros2)
         {
-            Console.WriteLine(c);
+            Console.WriteLine("Carro: {0}",c);
         }
+
+        string car="Golf";
+        int pos=0;
+        pos=carros.IndexOf(car);
+        Console.WriteLine("Carro {0} esta na posição {1}",car,pos);
     }
 }
